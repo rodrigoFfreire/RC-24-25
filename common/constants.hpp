adced8f851e087ded57d7ed9c33f39063fead79d
rodrigoFfreire <rodrigofreitasfreire@gmail.com>
2024-11-26 21:10:37 +0000

    refactor: shortened receive timeout

--- common/constants.hpp
+++ common/constants.hpp
@@ -12,7 +12,7 @@
 #define VALID_COLORS            "RGBYOP"
 
 #define SOCK_BUFFER_SIZE        128
-#define SERVER_RECV_TIMEOUT     10
+#define SERVER_RECV_TIMEOUT     3
 #define TCP_BACKLOG             5
 #define TCP_MAXCLIENTS          10
 #define TCP_CONN_RECV_TIMEOUT   10
